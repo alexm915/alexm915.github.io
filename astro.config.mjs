9c5c544e2732582b82472ad85f5fbacbbe566449
alex <aliasgmail@duck.com>
2026-06-07 18:00:44 +0800

    fix: set correct site URL in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'alexm915.github.io',
+	site: 'https://alexm915.github.io',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
